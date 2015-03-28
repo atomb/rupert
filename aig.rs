7d36d6a7bfdd3b4f42a5572273496f5ddc112f7d
Aaron Tomb <aarontomb@gmail.com>
2015-03-27 22:39:51 -0700

    Semi-complete AIGER parser
    Some parts still questionable and all parts untested.

--- aig.rs
+++ aig.rs
@@ -1,110 +1,266 @@
-enum AIGType { ASCII, Binary }
+#![allow(dead_code)] // TODO!
+#![allow(unused_variables)] // TODO!
+#![feature(str_words)]
+#![feature(core)]
 
-type latch = (uint, uint);
+use std::collections::HashMap;
+use std::str::StrExt;
+use std::str::Bytes;
 
-type gate = (uint, uint, uint);
+use AIGType::*;
+pub enum AIGType { ASCII, Binary }
+
+pub type Var = u64;
+pub type Lit = u64;
+pub type PosLit = u64;
+pub type DiffLit = u32;
+
+pub type Latch = (PosLit, Lit);
+pub type And = (PosLit, (Lit, Lit));
+pub type CompactAnd = (DiffLit, DiffLit);
 
 struct Header {
     aigtype: AIGType,
-    maxvar: uint,
-    ninputs: uint,
-    nlatches: uint,
-    noutputs: uint,
-    ngates: uint
+    maxvar: u64,
+    ninputs: u64,
+    nlatches: u64,
+    noutputs: u64,
+    nands: u64
+}
+
+struct AIGER {
+    header: Header,
+    body: AIG,
+    comments: Vec<String>
+}
+
+struct AIG {
+    inputs: Vec<PosLit>,
+    latches: Vec<(PosLit, Lit)>,
+    outputs: Vec<Lit>,
+    ands: HashMap<PosLit, (Lit, Lit)>
 }
 
 /*
-struct AIG {
-    inputs: Vec<uint>,
-    latches: Vec<uint>,
-    outputs: Vec<uint>,
-    gates: Vec<gate>
+struct CompactAIG {
+    inputs: Vec<PosLit>,
+    latches: Vec<Lit>,
+    outputs: Vec<Lit>,
+    ands: Vec<CompactAnd>
 }
 */
 
-fn parse_header(l: &str) -> Option<Header> {
+/*
+struct HashedAIG {
+    aig: AIG,
+    hash: HashMap<(Lit, Lit), Var>
+}
+*/
+
+static FALSE_LIT : Lit = 0;
+static TRUE_LIT  : Lit = 1;
+fn lit_sign   (l: Lit) -> bool { l & 1 == 1 }
+fn lit_strip  (l: Lit) -> Lit  { l & (!1) }
+fn lit_not    (l: Lit) -> Lit  { l ^ 1 }
+fn var_to_lit (v: Var) -> Lit  { v << 1 }
+fn lit_to_var (l: Lit) -> Var  { l >> 1 }
+/*
+fn mk_diff_lit(l: Lit, n: PosLit) -> DiffLit {
+    (var_to_lit((v - lit_to_var(l))) as u32) | ((l & 1) as u32)
+}
+
+fn compact_and(a: And) -> CompactAnd {
+    match a {
+        (n, (l, r)) => (mk_diff_lit(l, n), mk_diff_lit(r, n))
+    }
+}
+*/
+
+fn expand_and(a: CompactAnd, n: PosLit) -> And {
+    match a { (ld, rd) => (n, (n + ld as u64, n + rd as u64)) }
+}
+
+fn push_delta(delta: u32, v: &mut Vec<u8>) {
+    let mut tmp = delta;
+    while (tmp & !0x7f) != 0 {
+        v.push(((tmp & 0x7f) | 0x80) as u8);
+        tmp = tmp >> 7;
+    }
+    v.push(tmp as u8);
+}
+
+fn pop_delta(v: &mut Bytes) -> Option<u32> {
+    let mut x : u32 = 0;
+    let mut i : u8  = 0;
+    loop {
+        match v.next() {
+            Some(ch) => {
+                if ch & 0x80 == 0 {
+                    return Some (x | (ch << (7 * i)) as u32);
+                }
+                x = x | ((ch & 0x07f) << (7 * i)) as u32;
+                i = i + 1;
+            },
+            None => return None
+        }
+    }
+}
+
+fn parse_lit(s: &str) -> Result<Lit, String> {
+    match s.parse::<Lit>() {
+        Err(_) => Err("Failed to parse literal".to_string()),
+        Ok(l) => Ok(l)
+    }
+}
+
+fn parse_header(l: &str) -> Result<Header, String> {
     let mut ws = l.words();
     let ty =
         match ws.next() {
             Some("aig") => Binary,
             Some("aag") => ASCII,
-            _ => return None
+            _ => return Err("Invalid format identifier".to_string())
         };
     let rest: Vec<&str> = ws.collect();
-    match rest.as_slice() {
+    match &rest[..] {
         [mvs, nis, nls, nos, nas] => {
-            let mv = match from_str(mvs) { None => return None, Some(x) => x };
-            let ni = match from_str(nis) { None => return None, Some(x) => x };
-            let nl = match from_str(nls) { None => return None, Some(x) => x };
-            let no = match from_str(nos) { None => return None, Some(x) => x };
-            let na = match from_str(nas) { None => return None, Some(x) => x };
+            let mv = try!(parse_lit(mvs));
+            let ni = try!(parse_lit(nis));
+            let nl = try!(parse_lit(nls));
+            let no = try!(parse_lit(nos));
+            let na = try!(parse_lit(nas));
             let h = Header {
                 aigtype: ty,
                 maxvar: mv,
                 ninputs: ni,
                 nlatches: nl,
                 noutputs: no,
-                ngates: na
+                nands: na
             };
-            return Some(h)
+            return Ok(h)
         },
-        _ => return None
+        _ => return Err("Wrong number of words in header".to_string())
     }
 }
 
-fn parse_io(l: &str) -> Option<uint> {
+fn parse_io(l: &str) -> Result<u64, String> {
     let ws : Vec<&str> = l.words().collect();
-    match ws.as_slice() {
-        [ns] => from_str(ns),
-        _ => None
+    match &ws[..] {
+        [ns] => parse_lit(ns),
+        _ => Err("Wrong number of words on I/O line.".to_string())
     }
 }
 
-fn parse_latch_ascii(l: &str) -> Option<latch> {
+fn parse_latch_ascii(l: &str) -> Result<Latch, String> {
     let ws : Vec<&str> = l.words().collect();
-    match ws.as_slice() {
-        [is, ns] =>
-            match (from_str(is), from_str(ns)) {
-                (Some(i), Some(n)) => Some((i, n)),
-                _ => None
-            },
-        _ => None
+    match &ws[..] {
+        [is, ns] => {
+            let i = try!(parse_lit(is));
+            let n = try!(parse_lit(ns));
+            return Ok((i, n));
+        },
+        _ => Err("Wrong number of words on latch line.".to_string())
     }
 }
 
-fn parse_gate_ascii(l: &str) -> Option<gate> {
+fn parse_and_ascii(l: &str) -> Result<And, String> {
     let ws : Vec<&str> = l.words().collect();
-    match ws.as_slice() {
-        [ns, ls, rs] =>
-            match (from_str(ns), from_str(ls), from_str(rs)) {
-                (Some(n), Some(l), Some(r)) => Some((n, l, r)),
-                _ => None
-            },
-        _ => None
+    match &ws[..] {
+        [ns, ls, rs] => {
+            let n = try!(parse_lit(ns));
+            let l = try!(parse_lit(ls));
+            let r = try!(parse_lit(rs));
+            return Ok((n, (l, r)));
+        },
+        _ => Err("Wrong number of words on gate line.".to_string())
     }
 }
 
-fn parse_latch_binary(l: &str, i: uint) -> Option<latch> {
+fn parse_latch_binary(l: &str, i: u64) -> Result<Latch, String> {
     let ws : Vec<&str> = l.words().collect();
-    match ws.as_slice() {
-        [ns] =>
-            match from_str(ns) {
-                None => None,
-                Some(n) => Some((i, n))
-            },
-        _ => None
+    match &ws[..] {
+        [ns] => {
+            let n = try!(parse_lit(ns));
+            Ok((i, n))
+        }
+        _ => Err("Wrong number of words on binary latch line.".to_string())
     }
 }
 
-fn parse_gate_binary(l: &[u8]) -> Option<gate> {
-    None
+fn parse_cand_binary(l: &mut Bytes) -> Result<CompactAnd, String> {
+    match (pop_delta(l), pop_delta(l)) {
+        (Some(ld), Some(rd)) => Ok ((ld, rd)),
+        _ => Err("Failed to pop two deltas.".to_string())
+    }
+}
+
+pub fn parse_and_binary(n: PosLit, l: &mut Bytes) -> Result<And, String> {
+    let a = try!(parse_cand_binary(l));
+    Ok(expand_and(a, n))
+}
+
+fn parse_aiger(l: &str) -> Result<AIGER, String> {
+    let mut lns = l.lines();
+    let l = try!(lns.next().ok_or("No header".to_string()));
+    let h = try!(parse_header(l));
+    let mut is = Vec::with_capacity(h.ninputs as usize);
+    let mut ls = Vec::with_capacity(h.nlatches as usize);
+    let mut os = Vec::with_capacity(h.noutputs as usize);
+    let mut gs = HashMap::new(); // TODO: capacity?
+    for n in 0 .. h.ninputs {
+        let s = try!(lns.next().ok_or("Too few inputs".to_string()));
+        let i = try!(parse_io(s));
+        is.push(i);
+    }
+    for n in 0 .. h.nlatches {
+        let s = try!(lns.next().ok_or("Too few latches".to_string()));
+        let l =
+            match h.aigtype {
+                ASCII => try!(parse_latch_ascii(s)),
+                // TODO: is the node id in the following correct?
+                Binary => try!(parse_latch_binary(s, n + h.ninputs))
+            };
+        ls.push(l);
+    }
+    for n in 0 .. h.noutputs {
+        let s = try!(lns.next().ok_or("Too few outputs".to_string()));
+        let i = try!(parse_io(s));
+        os.push(i);
+    }
+    match h.aigtype {
+        ASCII => for n in 0 .. h.nands {
+            let s = try!(lns.next().ok_or("Too few ands".to_string()));
+            let (nid, a) = try!(parse_and_ascii(s));
+            gs.insert(nid, a);
+        },
+        Binary => {
+            let v = try!(lns.next().ok_or("Binary and block missing".to_string()));
+            let ref mut i = v.bytes();
+            for n in 0 .. h.nands {
+                // TODO: is nid correct here?
+                let nid = n + h.ninputs + h.nlatches + h.noutputs;
+                let (nid2, a) = try!(parse_and_binary(n, i));
+                gs.insert(nid2, a);
+            }
+        }
+    };
+    let aig =
+        AIG {
+            inputs: is,
+            outputs: os,
+            latches: ls,
+            ands: gs
+        };
+    let aiger =
+        AIGER {
+            header: h,
+            body: aig,
+            comments: vec![]
+        };
+    Ok(aiger)
 }
 
 /*
-fn parse_aiger(l: &str) -> Option<AIG> {
-    None
-}
-*/
-
 fn main() {
 }
+*/
