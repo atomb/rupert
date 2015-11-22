1b1b64ad4b449d97ac6992c3b0d4378f3385aa0d
Aaron Tomb <aarontomb@gmail.com>
2015-11-22 11:46:10 -0800

    Restructure dpll main function

--- src/bin/dpll.rs
+++ src/bin/dpll.rs
@@ -1,28 +1,27 @@
-//#![feature(exit_status)]
-
 extern crate rust_prover;
 
 use rust_prover::cnf;
 use rust_prover::dpll;
 use std::env;
+use std::process;
 use std::str;
 
 pub fn main() {
-    let vargs : Vec<String> = env::args().collect();
-    if vargs.len() != 2 {
+    if env::args().len() < 2 {
         println!("Usage: dpll <filename(s)>");
-        //env::set_exit_status(1);
-        return
+        process::exit(1);
     }
-    let ref name = vargs[1];
-    let mut contents : Vec<u8> = Vec::new();
-    match dpll::read_file(name, &mut contents) {
-        Err(e) => println!("Error reading file: {}", e),
-        Ok(_) => {
-            let s = str::from_utf8(contents.as_ref()).ok().unwrap();
-            match cnf::parse_dimacs_formula(s) {
-                None => println!("Failed to parse formula."),
-                Some(f) => dpll::do_dpll(f)
+    for name in env::args().skip(1) {
+        let mut contents : Vec<u8> = Vec::new();
+        println!("Processing {}", name);
+        match dpll::read_file(&name, &mut contents) {
+            Err(e) => println!("Error reading file {}: {}", name, e),
+            Ok(_) => {
+                let s = str::from_utf8(contents.as_ref()).ok().unwrap();
+                match cnf::parse_dimacs_formula(s) {
+                    None => println!("Failed to parse formula in {}.", name),
+                    Some(f) => dpll::do_dpll(f)
+                }
             }
         }
     }
